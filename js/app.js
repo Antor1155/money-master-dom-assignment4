864b402b5e550daf08d5eb3277c2a61ea55be2e3
Antor1155 <md.antor1155@gmail.com>
2022-02-17 22:06:08 +0800

    basic concept of js done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // function of validating the input and error message 
+// triggering the error messages 
 function numberValidate(string){
-    return parseInt(string);
+    return parseFloat(string);
 }
 
 // function to get value from any given id 
@@ -9,18 +10,55 @@ function idToValue(idName){
     return numberValidate(value);
 }
 
-// getting value form all input field 
+// function to set value in cost and balance fields 
+function setInnerText(idName, amount){
+    document.getElementById(idName).innerText = amount;
+}
 
-const income = idToValue("income");
+// making income and initial balance variable to use letter 
+let initialBalance = null;
+let income = null;
 
-const foodCost = idToValue("expenses1");
-const rentCost = idToValue("expenses2");
-const clothesCost = idToValue("expenses3");
 
-const savingPercent = idToValue("saving-percent")
+// calicking the calculate button and setting values
+document.getElementById('calculate').addEventListener('click',function(){
+    // getting value form all input field 
+    income = idToValue("income");
+    const foodCost = idToValue("expenses1");
+    const rentCost = idToValue("expenses2");
+    const clothesCost = idToValue("expenses3");
 
-// calculating initial cost and income 
-const initialCost = foodCost + rentCost + clothesCost;
-document.getElementById("totalExpenses").innerText = initialCost;
+    const initialCost = foodCost + rentCost + clothesCost;
+    initialBalance = income - initialCost ;
 
-console.log(income, foodCost, rentCost, clothesCost);
+    // initial expenses
+    setInnerText('totalExpenses', initialCost);
+    // initial balance
+    setInnerText('initialBalance', initialBalance);
+})
+
+// ************************************
+
+
+// clicking the saving button and setting final values
+document.getElementById('saveButton').addEventListener('click', function(){
+    const savingPercent = idToValue("saving-percent");
+    const savingAmount = income * (savingPercent/100);
+    const remainingBalance = initialBalance - savingAmount;
+
+    // saving amount
+    setInnerText('savingAmount', savingAmount);
+    // final balance
+    setInnerText('remainingBalance', remainingBalance);
+})
+
+
+
+// calculating cost, income
+
+
+
+// setting values to the costs and balance *************************
+
+
+// console.log(income, foodCost, rentCost, clothesCost);
