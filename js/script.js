5c2f3432b1457799fbde677215f17d11dfef987d
Antor1155 <md.antor1155@gmail.com>
2022-02-17 16:05:53 +0800

    general js implementation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+function elementToValue(input){
+    const val = document.getElementById(input);
+    return val;
+}
+document.getElementById('calculate').addEventListener("click", function (){
+    let x = document.getElementById('income').value;
+    console.log(x);
+})
